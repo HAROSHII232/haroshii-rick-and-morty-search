566a9fa82f6c991d50d247f7326f756c60d75dc5
HAROSHII <igorgrime@gmail.com>
2025-02-08 20:26:20 +0300

    ref: formatDate

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,3 @@
 export const formatDate = (dateString: string) => {
-  const date = new Date(dateString);
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const year = date.getFullYear();
-  return `${day}.${month}.${year}`;
+  return new Intl.DateTimeFormat("ru").format(new Date(dateString));
 };
