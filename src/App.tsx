8e31cd88a186ba4f4df907820f50a20b9f2c8df4
HAROSHII <igorgrime@gmail.com>
2025-02-08 21:08:34 +0300

    feat: add error showing if req fail

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,12 @@ import SearchBar from "./components/SearchBar/SearchBar";
 function App() {
   const [characters, setCharacters] = useState<Character[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
 
   const handleSearch = async (query: string) => {
     if (query.length <= 2) {
       setCharacters([]);
+      setError("");
       return;
     }
 
@@ -22,21 +24,22 @@ function App() {
       const response = await axios.get(
         `https://rickandmortyapi.com/api/character/?name=${query}`
       );
+      setError("");
       setCharacters(response.data.results);
     } catch (error: any) {
-      console.error("Error fetching data:", error.response.data.error);
+      setError(error.response.data.error);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div>
+    <>
       <SearchBar onSearch={handleSearch} foundCount={characters.length} />
       <div>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {characters && (
           <div>
             {characters.map((character) => (
               <CharacterCard key={character.id} character={character} />
@@ -44,7 +47,7 @@ function App() {
           </div>
         )}
       </div>
-    </div>
+    </>
   );
 }
 
